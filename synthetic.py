f8d4a6b7200f22b0afcb65d8be1eec7847c7b2f9
Meiliang Liu <145177424+MeiliangLiu@users.noreply.github.com>
2025-12-19 09:33:55 +0800

    Add files via upload

---
+++ synthetic.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.integrate import odeint
+
+def lorenz(x, t, F):
+    p = len(x)
+    dxdt = np.zeros(p)
+    for i in range(p):
+        dxdt[i] = (x[(i+1) % p] - x[(i-2) % p]) * x[(i-1) % p] - x[i] + F
+
+    return dxdt
+
+def simulate_lorenz_96(p, T, F, delta_t=0.1, sd=0.1, burn_in=1000,
+                       seed=0):
+    if seed is not None:
+        np.random.seed(seed)
+
+    x0 = np.random.normal(scale=0.01, size=p)
+    t = np.linspace(0, (T + burn_in) * delta_t, T + burn_in)
+    X = odeint(lorenz, x0, t, args=(F,))
+    X += np.random.normal(scale=sd, size=(T + burn_in, p))
+
+    # Set up Granger causality ground truth.
+    GC = np.zeros((p, p), dtype=int)
+    for i in range(p):
+        GC[i, i] = 1
+        GC[i, (i + 1) % p] = 1
+        GC[i, (i - 1) % p] = 1
+        GC[i, (i - 2) % p] = 1
+
+    return X[burn_in:], GC
